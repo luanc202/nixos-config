c1484052b5a5b20711db7c94ed3db818a6b3d28f
Luan Alves <barkon32@tuta.io>
2025-10-15 15:50:26 -0300

    feat: maybe this will fix

--- modules/programs/waybar.nix
+++ modules/programs/waybar.nix
@@ -1,8 +1,23 @@
-# modules/programs/waybar-hm.nix
+# Waybar config
 
 { pkgs, ... }:
 
 {
+  # Install waybar system-wide so it's available
+  environment.systemPackages = [ pkgs.waybar ];
+
+  # Apply the overlay to compile waybar with experimental features
+  nixpkgs.overlays = [
+    (self: super: {
+      waybar = super.waybar.overrideAttrs (oldAttrs: {
+        mesonFlags = oldAttrs.mesonFlags ++ [ "-Dexperimental=true" ];
+        patchPhase = ''
+          substituteInPlace src/modules/wlr/workspace_manager.cpp --replace "zext_workspace_handle_v1_activate(workspace_handle_);" "const std::string command = \"hyprctl dispatch workspace \" + name_; system(command.c_str());"
+        '';
+      });
+    })
+  ];
+
   # Configure the waybar program itself
   programs.waybar = {
     enable = true;
