4323f3609e47909cc771c36a1caaa531f879a973
Luan Alves <barkon32@tuta.io>
2025-10-15 17:44:26 -0300

    feat: move nixpkgs.overlay into hosts configuration.nix to maybe fix the warning

--- modules/programs/waybar.nix
+++ modules/programs/waybar.nix
@@ -4,16 +4,7 @@
 
 {
   # Apply the overlay to compile waybar with experimental features
-  nixpkgs.overlays = [
-    (self: super: {
-      waybar = super.waybar.overrideAttrs (oldAttrs: {
-        mesonFlags = oldAttrs.mesonFlags ++ [ "-Dexperimental=true" ];
-        patchPhase = ''
-          substituteInPlace src/modules/wlr/workspace_manager.cpp --replace "zext_workspace_handle_v1_activate(workspace_handle_);" "const std::string command = \"hyprctl dispatch workspace \" + name_; system(command.c_str());"
-        '';
-      });
-    })
-  ];
+
 
   # Configure the waybar program itself
   programs.waybar = {
