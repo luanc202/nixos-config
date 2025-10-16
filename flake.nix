d96ca502943af6ba1417f3bb246f84c681eef73d
Luan Alves <barkon32@tuta.io>
2025-10-15 22:22:16 -0300

    feat: revert back to unstable version

--- flake.nix
+++ flake.nix
@@ -11,7 +11,7 @@
 
   inputs =                                                                  # All flake references used to build my NixOS setup. These are dependencies.
     {
-      nixpkgs.url = "github:nixos/nixpkgs/unstable";                  # Nix Packages
+      nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";                  # Nix Packages
 
       home-manager = {                                                      # User Package Management
               url = "github:nix-community/home-manager";
