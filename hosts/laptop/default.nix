1811dde58d7b70786d15f369907520b478d7eea6
Luan Alves <barkon32@tuta.io>
2025-10-19 19:46:39 +0000

    feat: set locale default to ptbr

--- hosts/laptop/default.nix
+++ hosts/laptop/default.nix
@@ -36,6 +36,14 @@
         keyMap = "br-abnt2";                          # or us/azerty/etc
     };
 
+    time.timeZone = "Brazil/Fortaleza";        # Time zone and internationalisation
+    i18n = {
+      defaultLocale = "pt_BR.UTF-8";
+      extraLocaleSettings = {                 # Extra locale settings that need to be overwritten
+        LC_TIME = "pt_BR.UTF-8";
+        LC_MONETARY = "pt_BR.UTF-8";
+      };
+
   boot = {                                      # Boot options
     kernelPackages = pkgs.linuxPackages_latest;
     # initrd.kernelModules = [ "amdgpu" ];       # Video drivers
